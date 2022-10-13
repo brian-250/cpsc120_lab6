7261524bb58ced685f13b3304a0ac69b2b3c14f1
Max Rivas <Rivas.max11@csu.fullerton.edu>
2022-10-13 14:24:35 -0700

    Properly creates the pattern and utilizes a nested loop to print the bars and dashes'

--- part-1/pattern.cc
+++ part-1/pattern.cc
@@ -1,24 +1,28 @@
-
-// TODO: Insert your header.
+// Max Rivas
+// CPSC 120-01
+// 2022-10-13
+// Rivas.max11@csu.fullerton.edu
+// @Mrivas0331
+//
+// Lab 06-01
+// Partners: @brian-250
+//
+// Creates pattern
+//
 
 #include <iostream>
 
-// TODO: Declare a const int named kCounterMax and initialize it to 22.
-
+const int kCounterMax = 22;
 int main(int argc, char const *argv[]) {
-  // TODO: Write an outer loop which starts from 0 and goes up to the counter
-  // max.
-
-  // TODO: Write an inner loop which starts from the current line number and
-  // counts down to zero. Make sure that this loop is inside the outer loop.
-
-  // TODO: Print an asterisk.
-
-  // TODO: Write another inner loop which starts from the current line number
-  // and counts up to the counter max. Make sure that this loop is inside the
-  // outer loop but outside the first inner loop.
-
-  // TODO: print a new line character
-
+  for (int outer = 0; outer < kCounterMax; outer++) {
+    for (int dash = outer; dash > 0; dash--) {
+      std::cout << "-";
+    }
+    std::cout << "*";
+    for (int bar = outer; bar < kCounterMax; bar++) {
+      std::cout << "|";
+    }
+    std::cout << "\n";
+  }
   return 0;
 }
